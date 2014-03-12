e440e30ff841236cafa126d30ab087c72004a658
Fernando Chorney <github@djsbx.com>
2014-03-11 22:19:07 -0500

    Added GPIO mapping to HelloWorld Exampls

--- examples/HelloWorld.py
+++ examples/HelloWorld.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python
 
 import RPIO
-
+import time
 from RPiLiquidCrystal import LCD
+
+PIN = {
+    'GPIO0':   3,
+    'GPIO1':   5,
+    'GPIO4':   7,
+    'GPIO7':  26,
+    'GPIO8':  24,
+    'GPIO9':  21,
+    'GPIO10': 19,
+    'GPIO11': 23,
+    'GPIO14':  8,
+    'GPIO15': 10,
+    'GPIO17': 11,
+    'GPIO18': 12,
+    'GPIO21': 13,
+    'GPIO22': 15,
+    'GPIO23': 16,
+    'GPIO24': 18,
+    'GPIO25': 22,
+}
 
 def main():
     # Rewrite pins for your own specification
-    rs = 0
-    enable = 1
-    d4 = 2
-    d5 = 3
-    d6 = 4
-    d7 = 5
+    RS      = PIN['GPIO23']
+    ENABLE  = PIN['GPIO24']
+    D4      = PIN['GPIO17']
+    D5      = PIN['GPIO4']
+    D6      = PIN['GPIO22']
+    D7      = PIN['GPIO18']
     try:
-        RPIO.setmode(RPIO.BCM)
+        RPIO.setmode(RPIO.BOARD)
         lcd = LCD(
-            rs, enable, [d4, d5, d6, d7],
+            RS, ENABLE, [D4, D5, D6, D7],
             lines=2, fourbitmode=True
         )
-        lcd.clear()
-        lcd.home()
         lcd.setCursor(0, 0)
         lcd.printMessage('Hello')
         lcd.setCursor(0, 1)
         lcd.printMessage('World')
+        time.sleep(10)
     except KeyboardInterrupt:
         pass
     RPIO.cleanup()
