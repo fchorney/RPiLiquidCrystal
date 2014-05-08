c8847dd548134049018abe5799d8ea19788cf714
Fernando Chorney <github@djsbx.com>
2014-05-08 10:23:19 -0500

    added some initialization code for i2c as a test.

--- RPiLiquidCrystal/LCD_i2C.py
+++ RPiLiquidCrystal/LCD_i2C.py
@@ -10,32 +10,39 @@ class i2c_device:
         self.port = port
         self.bus = smbus.SMBus(port)
 
+
     # Write a single command
     def write_cmd(self, cmd):
         self.bus.write_byte(self.addr, cmd)
         delay_microseconds(100)
+
 
     # Write a command and argument
     def write_cmd_arg(self, cmd, data):
         self.bus.write_byte_data(self.addr, cmd, data)
         delay_microseconds(100)
 
+
     # Write a block of data
     def write_block_data(self, cmd, data):
         self.bus.write_block_data(self.addr, cmd, data)
         delay_microseconds(100)
 
+
     # Read a single byte
     def read(self):
         return self.bus.read_byte(self.addr)
+
 
     # Read
     def read_data(self, cmd):
         return self.bus.read_byte_data(self.addr, cmd)
 
+
     # Read a block of data
     def read_block_data(self, cmd):
         return self.bus.read_block_data(self.addr, cmd)
+
 
     # Clean up
     def cleanup(self):
@@ -53,7 +60,8 @@ class LCD_Protocol():
 
     def __init__(self, i2c_addr, cols=16, lines=1, dotsize=0):
         # Internal Variables
-        self.__i2c_bus = i2c_device(i2c_addr)
+        self.__i2c_addr = i2c_addr
+        self.__i2c_bus = None
 
         # Public Variables
         self.numlines = lines
@@ -63,11 +71,14 @@ class LCD_Protocol():
 
     # Protocol Functions
     def initialize(self):
-        pass
+        self.__i2c_bus = i2c_device(self.__i2c_addr)
 
 
     def begin(self):
-        pass
+        self.write4bits(0)
+        delay_microseconds(5)
+        self.write4bits(0)
+        delay_microseconds(5)
 
 
     def send(self, value, mode):
@@ -79,7 +90,7 @@ class LCD_Protocol():
         self.__i2c_bus.write_cmd(value | LCD_Protocol.BACKLIGHT)
         self.__pulse_enable(value)
 
-
+    # XXX: Figure out how to control the backlight through i2C code
     def enableBacklight(self, intensity=100):
         pass
 
